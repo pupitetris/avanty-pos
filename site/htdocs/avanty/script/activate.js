b723543cccd99b54b19ef068bdf47234967e5c00
Arturo Espinosa <arturoea@gmail.com>
2017-10-06 16:17:52 -0500

    activate: finished activation greeting screen, first screen of the application.

--- site/htdocs/avanty/script/activate.js
+++ site/htdocs/avanty/script/activate.js
@@ -7,7 +7,26 @@
 	var MOD_NAME = 'activate';
 
 	var activate_sequence = [1,2,3,4,5];
-	var activate_current = -1;
+	var activate_current = 0;
+
+	function quadrant_click (evt) {
+		if (activate_current == activate_sequence.length)
+			// Sequence has been entered. Continue.
+			return;
+		
+		var id = evt.currentTarget.id;
+		var num = parseInt (id.split ('-')[1], 10);
+		if (activate_sequence[activate_current] != num) {
+			activate_current = 0;
+			return;
+		}
+
+		activate_current ++;
+		if (activate_current < activate_sequence.length)
+			return;
+
+		mod.greeting_cont.fadeIn (1000);
+	}
 
 	var mod = {
 		init: function () {
@@ -19,12 +38,22 @@
 			if (!mod.loaded)
 				return;
 
+			$('.activate-quadrant').bind ('click', quadrant_click);
+
+			mod.greeting_cont = $('#activate-greeting-continue');
+			
+			var greeting_button = mod.greeting_cont.find ('button');
+			greeting_button.button ();
+
 			APP.switchPage (MOD_NAME);
 			APP.switchSection ($('#activate-greeting'), $('#activate-sections'));
+
+			mod.reset ();
 		},
 
 		reset: function () {
-			activate_current = -1;
+			activate_current = 0;
+			mod.greeting_cont.hide ();
 		}
 	};
 

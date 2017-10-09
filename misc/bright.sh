52b278e14ab40ed7d2ff43ec5e888853425bb54c
Arturo Espinosa <arturoea@gmail.com>
2017-10-09 14:29:39 -0500

    A series of files used in Linux to overcome virtual keyboard issues.

---
+++ misc/bright.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+
+statefile=$HOME/.bright
+inc=$1
+
+[ -e $statefile ] || echo 10 > $statefile
+
+curr=$(cat $statefile)
+
+if ((curr==10&&inc>0)); then let inc*=3; fi
+if ((curr>10)); then let inc*=3; fi
+let curr=curr+inc
+
+if ((curr<=3)); then curr=3; fi
+if ((curr>=40)); then curr=40; fi
+
+echo $curr > $statefile
+
+if ((curr<10)); then curr=0$curr; fi
+
+xrandr --output eDP1 --brightness ${curr:0:1}.${curr:1:2}

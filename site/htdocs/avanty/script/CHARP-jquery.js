1289e87542cc1c9e90dc38e5cb5a73629964578b
Arturo Espinosa <arturoea@gmail.com>
2017-10-12 19:51:16 -0500

    CHARP-jquery.js: tab conversion and (C) header.

--- site/htdocs/avanty/script/CHARP-jquery.js
+++ site/htdocs/avanty/script/CHARP-jquery.js
@@ -1,28 +1,25 @@
-// This file is part of the CHARP project.
+// This file is part of Microsafe AVANTY. -*- tab-width: 4; -*-
 //
-// Copyright © 2011 - 2014
-//   Free Software Foundation Europe, e.V.,
-//   Talstrasse 110, 40217 Dsseldorf, Germany
-//
-// Licensed under the EUPL V.1.1. See the file LICENSE.txt for copying conditions.
+// Copyright © 2017 Microsafe, S.A. de C.V.
+// Derechos Reservados (R) 2017 Microsafe, S.A. de C.V.
 
 CHARP.extendObj = function (obj, add) {
-    return $.extend ({}, obj, add);
+	return $.extend ({}, obj, add);
 }
 
 CHARP.paramsUriEncode = function (params) {
-    return $.param (params);
+	return $.param (params);
 }
 
 CHARP.ajaxPost = function (url, params, successCb, completeCb) {
-    $.ajax ({ 
-	type: 'POST',
-	url: url,
-	cache: false,
-	data: params,
-	dataType: 'json',
-	global: false,
-	success: successCb,
-	complete: completeCb,
-    });
+	$.ajax ({ 
+		type: 'POST',
+		url: url,
+		cache: false,
+		data: params,
+		dataType: 'json',
+		global: false,
+		success: successCb,
+		complete: completeCb,
+	});
 }

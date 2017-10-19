ba6d937f86c3a6a5f1ddda1cb4c78b7948e6629f
Arturo Espinosa <arturoea@gmail.com>
2017-10-19 12:59:35 -0500

    CHARP-config.pm: new tracing configuration variables.

--- site/cgi/CHARP-config.pm
+++ site/cgi/CHARP-config.pm
@@ -34,4 +34,18 @@ $DB_DRIVER = 'Pg';
 # Language for the localized strings.
 $CHARP_LANG = 'es';
 
+# DBI tracing:
+# http://search.cpan.org/~timb/DBI-1.637/DBI.pm#TRACING
+
+# No tracing (production):
+#$DB_TRACE = '0';
+
+# Trace top-level DBI method calls returning with results or errors and SQL statements executed:
+$DB_TRACE = '0|SQL';
+$DB_TRACE_FNAME = *STDERR;
+
+# Send errors to stderr? (Effectively showing on the web server's error.log)
+# $DB_PRINT_ERROR = 0; # Production
+$DB_PRINT_ERROR = 1;
+
 1;

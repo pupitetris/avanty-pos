95956eb88dc1b975fdbfbc6e3f2f9cc68f5ce207
Arturo Espinosa <arturoea@gmail.com>
2017-11-08 22:58:50 -0600

    initdb.sql: added test rate insertions.

--- Database/initdb.sql
+++ Database/initdb.sql
@@ -98,4 +98,75 @@ zozCzYdSCBfnRztD8A==
 		   (DEFAULT,  50000, 'bill'),
 		   (DEFAULT, 100000, 'bill');
     
+	DELETE FROM rate;
+    ALTER SEQUENCE rate_rate_id_seq RESTART;
+	INSERT INTO rate VALUES (DEFAULT, 'regular', 'test', '// Fuente: http://www.oma.aero/es/aeropuertos/monterrey/pasajeros/servicios/estacionamiento.htm
+
+incluir test-desc.fth
+incluir test-param.fth
+incluir test-calcular.fth
+
+calcular
+', NULL);
+   INSERT INTO rate VALUES (DEFAULT, 'regular', 'test-desc', 'define desc_primera_hora " La primera hora o fracción " ;
+define desc_fracción " Fracción de 30min " ;
+define desc_tarifa_diaria_máxima " Cobro máximo permitido " ;
+define desc_extraviado " Boleto extraviado " ;
+', NULL);
+   INSERT INTO rate VALUES (DEFAULT, 'regular', 'test-param', 'define costo_primera_hora 32 pesos ;
+define costo_fracción 16 pesos ;
+define costo_extraviado 360 pesos ;
+
+define duración_fracción 30 minutos ; 
+define tarifa_diaria_máxima 360 pesos ;
+', NULL);
+   INSERT INTO rate VALUES (DEFAULT, 'regular', 'test-calcular', '// Declaramos las variables que vamos a usar:
+
+variable restante   // Será el tiempo registrado menos la primer hora.
+variable fracciones // Número de fracciones de tiempo por cobrar.
+variable total      // El total, para ver si nos pasamos de la tarifa diara máxima.
+
+define calcular
+
+// Checa la existencia de la constante "extraviado" y si es así, se cobra el concepto
+// de boleto extraviado y termina.
+si está extraviado entonces
+	1 costo_extraviado desc_extraviado registra
+	salir
+fin
+
+// Se cobra la primer hora o fracción:
+1 costo_primera_hora desc_primera_hora registra
+
+// Si el tiempo registrado es menor a una hora, terminamos.
+si tiempo_registrado 1hr <= entonces
+	salir
+fin
+
+// El tiempo restante es el tiempo registrado menos una hora.
+tiempo_registrado 1hr - restante guarda
+
+// Calculamos cuántas fracciones adicionales se registraron:
+restante valor duración_fracción / fracciones guarda
+
+// Si no se registró la salida en el minuto exacto de la última fracción, sumar una fracción adicional.
+si restante valor duración_fracción residuo 0 > entonces
+	fracciones incrementa
+fin
+
+fracciones valor costo_fracción desc_fracción registra
+
+// Calcula el total: número de fracciones por su costo mas lo de la primer hora.
+fracciones valor costo_fracción * costo_primera_hora + total guarda
+
+// Si el total obtenido es mayor a la tarifa máxima diaria, marcamos lo ya reportado como cancelado
+// y reportamos la tarifa máxima
+si total valor tarifa_diaria_máxima > entonces
+	cancela_anteriores
+	1 tarifa_diaria_máxima desc_tarifa_diaria_máxima registra
+fin
+
+;
+', NULL);
+
 COMMIT TRANSACTION;

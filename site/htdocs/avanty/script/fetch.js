ab64ee82df4f8bdbb704a1c4baa3b02ef6da6c95
Arturo Espinosa <arturoea@gmail.com>
2017-11-13 19:10:15 -0600

    fetch.js: we're not using this yet, but reflected APP.config.DEVEL change, and indentation to 4-space tabs.

--- site/htdocs/avanty/script/fetch.js
+++ site/htdocs/avanty/script/fetch.js
@@ -1,67 +1,61 @@
-// This file is part of the CHARP project.
+// This file is part of Microsafe AVANTY. -*- tab-width: 4; -*-
 //
-// Copyright © 2011 - 2014
-//   Free Software Foundation Europe, e.V.,
-//   Talstrasse 110, 40217 Dsseldorf, Germany
-//
-// Licensed under the EUPL V.1.1. See the file LICENSE.txt for copying conditions.
-
-// This module implements a simple caching fetcher to get data from the DB that doesn't change
-// much, such as catalogs.
+// Copyright © 2017 Microsafe, S.A. de C.V.
+// Derechos Reservados (R) 2017 Microsafe, S.A. de C.V.
 
 (function () {
-    var cache = {};
+	var cache = {};
 
-    function fetch_success (data, ctx) {
-	var path = ctx.path;
-	var parent;
-	var node = cache;
-	for (var i = 0, key; key = path[i]; i++) {
-	    if (!node[key] && path[i + 1])
-		node[key] = {};
-	    parent = node;
-	    node = node[key];
+	function fetch_success (data, ctx) {
+		var path = ctx.path;
+		var parent;
+		var node = cache;
+		for (var i = 0, key; key = path[i]; i++) {
+			if (!node[key] && path[i + 1])
+				node[key] = {};
+			parent = node;
+			node = node[key];
+		}
+		parent[path.pop ()] = data;
+
+		ctx.cb (data);
 	}
-	parent[path.pop ()] = data;
 
-	ctx.cb (data);
-    }
-
-    function fetch_from_cache (path) {
-	var parent;
-	var node = cache;
-	for (var i = 0, key; key = path[i]; i++) {
-	    node = node[key];
-	    if (!node)
-		return undefined;
+	function fetch_from_cache (path) {
+		var parent;
+		var node = cache;
+		for (var i = 0, key; key = path[i]; i++) {
+			node = node[key];
+			if (!node)
+				return undefined;
+		}
+		return node;
 	}
-	return node;
-    }
 
-    var mod = function (proc, area, params, asAnon, cb) {
-	if (!params)
-	    params = [];
-	var path = [area, proc].concat (params);
-	var data = fetch_from_cache (path);
-	if (data)
-	    cb (data);
-	else
-	    APP.charp.request (proc, params, { success: fetch_success, cb: cb, path: path, asAnon: asAnon });
-    };
+	var mod = function (proc, area, params, asAnon, cb) {
+		if (!params)
+			params = [];
+		var path = [area, proc].concat (params);
+		var data = fetch_from_cache (path);
+		if (data)
+			cb (data);
+		else
+			APP.charp.request (proc, params, { success: fetch_success, cb: cb, path: path, asAnon: asAnon });
+	};
 
-    mod.init = function () {
-	    mod.initialized = true;
-    };
+	mod.init = function () {
+			mod.initialized = true;
+	};
 
-    mod.cacheDump = function (area) {
-	if (!area)
-	    cache = {};
-	else if (mod.cache[area])
-	    cache[area] = {};
-    };
+	mod.cacheDump = function (area) {
+		if (!area)
+			cache = {};
+		else if (mod.cache[area])
+			cache[area] = {};
+	};
 
-    if (APP.DEVEL)
-	mod.cache = cache;
-	
-    APP.fetch = mod;
+	if (APP.config.DEVEL)
+		mod.cache = cache;
+		
+	APP.fetch = mod;
 }) ();

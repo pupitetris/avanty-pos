7a0beeb9b82059aecc2724e3449cc5ebbc219664
Arturo Espinosa <arturoea@gmail.com>
2017-12-26 18:22:44 -0600

    cash.js: fetch rates catalog and report per-rate amounts and counts.

--- site/htdocs/avanty/script/cash.js
+++ site/htdocs/avanty/script/cash.js
@@ -875,65 +875,92 @@
 	}
 
 	function cash_shift_do_report (records, prefix) {
-		var desc = {
-			shift_begin: 'Dotación inicial:',
-			deposit: 'Depósito:'
-		};
-
-		var total = 0;
-		var change = 0;
-		var tickets = 0;
-		var pre = '';
-		ui[prefix + '_table'].empty ();
-		for (var rec of records) {
-			if (rec.change) change += rec.change;
-
-			switch (rec.concept) {
-			case 'exit':
-			case 'lost':
-				tickets ++;
-				total += rec.amount;
-				break;
-			case 'shift_begin':
-				total += rec.amount;
-				ui.tickets[prefix + '_begin_time'].text (rec.timestamp.toLocaleString ());
-				break;
-			case 'shift_end':
-				ui.tickets[prefix + '_num'].text (rec.amount);
-				ui.tickets[prefix + '_time'].text (rec.timestamp.toLocaleString ());
+		APP.fetch ('rates_get', 'CASH', [], true, function (rate_data) {
+			var rates = {};
+			for (var rate of rate_data) {
+				rates[rate[name]] = rate;
+				rate.count = 0;
+				rate.amount = 0;
 			}
-			if (desc[rec.concept]) {
-				pre += '<div class="desc">' + rec.timestamp.toLocaleString () + ' ' + desc[rec.concept] + '</div>\n' +
-					'<div class="sum">$' + APP.Util.asMoney (rec.amount) + '</div>\n';
-				ui[prefix + '_table'].append ($('<tr>' +
-											 '<td>' + rec.timestamp.toLocaleString () + '</td>' +
-											 '<th>' + desc[rec.concept] + '</th>' +
-											 '<td><s/></td><td class="money">' + APP.Util.asMoney (rec.amount) + '</td>' +
-											 '</tr>'));
+			
+			var desc = {
+				shift_begin: 'Dotación inicial:',
+				deposit: 'Depósito:'
+			};
+
+			var total = 0;
+			var change = 0;
+			var tickets = 0;
+			var pre = '';
+			ui[prefix + '_table'].empty ();
+			for (var rec of records) {
+				if (rec.change) change += rec.change;
+
+				if (rec.rate) {
+					var rate = rates[rec.rate.name];
+					rate.count ++;
+					rate.amount += rec.amount;
+				}
+
+				switch (rec.concept) {
+				case 'exit':
+				case 'lost':
+					tickets ++;
+					total += rec.amount;
+					break;
+				case 'shift_begin':
+					total += rec.amount;
+					ui.tickets[prefix + '_begin_time'].text (rec.timestamp.toLocaleString ());
+					break;
+				case 'shift_end':
+					ui.tickets[prefix + '_num'].text (rec.amount);
+					ui.tickets[prefix + '_time'].text (rec.timestamp.toLocaleString ());
+				}
+
+				if (desc[rec.concept]) {
+					pre += '<div class="desc">' + rec.timestamp.toLocaleString () + ' ' + desc[rec.concept] + '</div>\n' +
+						'<div class="sum">$' + APP.Util.asMoney (rec.amount) + '</div>\n';
+					ui[prefix + '_table'].append ($('<tr>' +
+													'<td>' + rec.timestamp.toLocaleString () + '</td>' +
+													'<th>' + desc[rec.concept] + '</th>' +
+													'<td><s/></td><td class="money">' + APP.Util.asMoney (rec.amount) + '</td>' +
+													'</tr>'));
+				}
 			}
-		}
 
-		var received = total + change;
+			for (var rate of rate_data) {
+				pre += '<div class="desc">Tarifa: ' + rate.label_client + '</div>\n' +
+					'<div class="sum">' + rate.count + ': $' + APP.Util.asMoney (rate.amount) + '</div>\n';
+				ui[prefix + '_table'].append ($('<tr><th>Tarifas</th><td colspan="3"></td></tr>' + 
+												'<tr>' +
+												'<td>' + rate.count + '</td>' +
+												'<th>' + rate.label + '</th>' +
+												'<td><s/></td><td class="money">' + APP.Util.asMoney (rate.amount) + '</td>' +
+												'</tr>'));
+			}
 
-		received = APP.Util.asMoney (received);
-		pre += '<div class="sum">Recibido: $' + received + '</div><br />\n';
-		ui[prefix + '_received'].text (received);
+			var received = total + change;
 
-		change = APP.Util.asMoney (change);
-		pre += '<div class="sum">Devuelto: $' + change + '</div><br />\n';
-		ui[prefix + '_change'].text (change);
+			received = APP.Util.asMoney (received);
+			pre += '<div class="sum">Recibido: $' + received + '</div><br />\n';
+			ui[prefix + '_received'].text (received);
 
-		total = APP.Util.asMoney (total);
-		pre += '<br /><div class="sum">Cobrado: $' + total + '</div>\n';
-		ui[prefix + '_total'].text (total);
+			change = APP.Util.asMoney (change);
+			pre += '<div class="sum">Devuelto: $' + change + '</div><br />\n';
+			ui[prefix + '_change'].text (change);
 
-		pre += '<div class="sum">Boletos cobrados: ' + tickets + '</div>';
-		ui[prefix + '_tickets'].text (tickets);
+			total = APP.Util.asMoney (total);
+			pre += '<br /><div class="sum">Cobrado: $' + total + '</div>\n';
+			ui[prefix + '_total'].text (total);
 
-		ui.tickets[prefix + '_terminal'].text (APP.terminal.name);
-		ui.tickets[prefix + '_user'].text (APP.charp.credentialsGet ().login);
-		ui.tickets[prefix + '_items'].html (pre);
-		APP.mod.devices.escposTicketLayout (ui.tickets[prefix]);
+			pre += '<div class="sum">Boletos cobrados: ' + tickets + '</div>';
+			ui[prefix + '_tickets'].text (tickets);
+
+			ui.tickets[prefix + '_terminal'].text (APP.terminal.name);
+			ui.tickets[prefix + '_user'].text (APP.charp.credentialsGet ().login);
+			ui.tickets[prefix + '_items'].html (pre);
+			APP.mod.devices.escposTicketLayout (ui.tickets[prefix]);
+		});
 	}
 
 	function cash_shift_report_print () {

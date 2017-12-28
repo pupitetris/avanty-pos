953c0cf12248d120ce160d991c4a73b8b13a832b
Arturo Espinosa <arturoea@gmail.com>
2017-12-27 22:36:19 -0600

    avanty.dbm, report.js: shift reporting fixes.

--- site/htdocs/avanty/script/report.js
+++ site/htdocs/avanty/script/report.js
@@ -7,6 +7,46 @@
 (function () {
 	var MOD_NAME = 'report';
 
+	function render_shift_ids (shift_ids) {
+		if (shift_ids.length == 0)
+			return '---';
+
+		var shift_str = '';
+
+		var end = shift_ids[0];
+		var count = 1;
+
+		shift_str += end;
+
+		for (var i = 1, id = shift_ids[1]; id; id = shift_ids[++i]) {
+			if (id == end + 1) {
+				end = id;
+				count ++;
+			}
+			if (id > end + 1 || i == shift_ids.length - 1) {
+
+				var post = ' ' + id;
+				if (i == shift_ids.length - 1)
+					post = '';
+
+				switch (count) {
+				case 1:
+					shift_str += ' ' + id;
+					break;
+				case 2:
+					shift_str += ' ' + end + post;
+					break;
+				default:
+					shift_str += '-' + end + post;
+				}
+				end = id;
+				count = 1;
+			}
+		}
+
+		return shift_str;
+	}
+
 	var mod = {
 		init: function () {
 			mod.initialized = true;
@@ -45,11 +85,10 @@
 				var printed_tickets = 0;
 				var pre = '';
 				var shift_begin_ts;
+				var shift_ids = [];
 
 				ui[prefix + '_table'].empty ();
 				for (var rec of records) {
-					if (rec.change) change += rec.change;
-
 					if (rec.rate) {
 						var rate = rates[rec.rate.name];
 						rate.count ++;
@@ -59,25 +98,32 @@
 					switch (rec.concept) {
 					case 'entry':
 						printed_tickets ++;
-						break;
+						if (!rec.amount)
+							break;
 					case 'exit':
 					case 'lost':
 						charged_tickets ++;
 						charged += rec.amount;
+						if (rec.change)
+							change += rec.change;
 						break;
 					case 'shift_begin':
 						num_deposits ++;
 						deposit += rec.amount;
+						shift_ids.push (rec.change);
 						if (num_deposits == 1) {
 							shift_begin_ts = rec.timestamp.toLocaleString ();
 							if (ui.tickets[prefix + '_begin_time'])
-								ui.tickets[prefix + '_begin_time'].text (rec.timestamp.toLocaleString ());
+								ui.tickets[prefix + '_begin_time'].text (shift_begin_ts);
 						} else
 							shift_begin_ts = '';
 						break;
 					case 'shift_end':
 						ui.tickets[prefix + '_num'].text (rec.amount);
 						ui.tickets[prefix + '_time'].text (rec.timestamp.toLocaleString ());
+						break;
+					default:
+						console.warn ('Unknown concept ' + rec.concept);
 					}
 
 					if (desc[rec.concept]) {
@@ -91,6 +137,12 @@
 					}
 				}
 
+				var shift_ids_str = render_shift_ids (shift_ids);
+				if (ui.tickets[prefix + '_shift_id'])
+					ui.tickets[prefix + '_shift_id'].text (shift_ids_str);
+				if (ui[prefix + '_shift_id'])
+					ui[prefix + '_shift_id'].text (shift_ids_str);
+
 				pre += '<div class="desc">' + shift_begin_ts + ' Dotación:</div>\n' +
 					'<div class="sum">$' + APP.Util.asMoney (deposit) + '</div>\n';
 				ui[prefix + '_table'].append ($('<tr>' +
@@ -99,7 +151,7 @@
 												'<td><s/></td><td class="money">' + APP.Util.asMoney (deposit) + '</td>' +
 												'</tr>'));
 				
-				ui[prefix + '_table'].append ($('<tr><th>Tarifas:</th><td colspan="3"></td></tr>'));
+				ui[prefix + '_table'].append ($('<tr><th>Cobros por tarifa:</th><td colspan="3"></td></tr>'));
 				for (var rate of rate_data) {
 					pre += '<div class="desc">Tarifa: ' + rate.label_client + '</div>\n' +
 						'<div class="sum">(' + rate.count + ') $' + APP.Util.asMoney (rate.amount) + '</div>\n';
@@ -113,7 +165,7 @@
 				var received = charged + deposit + change;
 
 				received = APP.Util.asMoney (received);
-				pre += '<div class="sum">Recibido: $' + received + '</div><br />\n';
+				pre += '<br /><div class="sum">Recibido: $' + received + '</div><br />\n';
 				ui[prefix + '_received'].text (received);
 
 				change = APP.Util.asMoney (change);
@@ -121,7 +173,7 @@
 				ui[prefix + '_change'].text (change);
 
 				charged = APP.Util.asMoney (charged);
-				pre += '<br /><div class="sum">Cobrado: $' + charged + '</div>\n';
+				pre += '<div class="sum">Cobrado: $' + charged + '</div>\n';
 				ui[prefix + '_charged'].text (charged);
 
 				pre += '<div class="sum">Boletos cobrados: ' + charged_tickets + '</div>';
